72ada709ae77fd323184e47bc86ce326e83afe8d
niperia <haytamaatita2001@gmail.com>
2024-09-19 14:46:39 +0100

    commit

--- app/produits/[slug]/page.jsx
+++ app/produits/[slug]/page.jsx
@@ -324,35 +324,33 @@ export default function Example({ params }) {
                     <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
                       {Produits.map((product) => (
                         <div key={product.id} className="productCard">
-                          <Link href={`/${product.name}`}>
-                            <div className="card flex flex-col justify-center p-3 items-center text-center bg-white rounded-lg shadow-2xl">
-                              <div className=" w-40 min-h-40 flex text-center  justify-center">
-                                <img
-                                  src={`http://localhost:8080/api/images/${product.picture}`}
-                                  className="object-contain object-center"
-                                  alt={product.title}
-                                />
-                              </div>
-                              <div className="prod-title mb-4 ">
-                                <p className="text-xl uppercase text-gray-900 font-bold">
-                                  {product.name}
+                          <div className="card flex flex-col justify-center p-3 items-center text-center bg-white rounded-lg shadow-2xl">
+                            <div className=" w-40 min-h-40 flex text-center  justify-center">
+                              <img
+                                src={`http://localhost:8080/api/images/${product.picture}`}
+                                className="object-contain object-center"
+                                alt={product.title}
+                              />
+                            </div>
+                            <div className="prod-title mb-4 ">
+                              <p className="text-xl uppercase text-gray-900 font-bold">
+                                {product.name}
+                              </p>
+                              <p className="uppercase text-sm text-gray-400">
+                                {product.marque}
+                              </p>
+                            </div>
+                            <div className="prod-info grid gap-4">
+                              <div className="flex flex-col  justify-between items-center text-gray-900">
+                                <p className="font-bold text-xl px-2">
+                                  {product.price} DH
                                 </p>
-                                <p className="uppercase text-sm text-gray-400">
-                                  {product.marque}
-                                </p>
-                              </div>
-                              <div className="prod-info grid gap-4">
-                                <div className="flex flex-col  justify-between items-center text-gray-900">
-                                  <p className="font-bold text-xl px-2">
-                                    {product.price} DH
-                                  </p>
-                                  <button className="px-6 py-2 mt-2 md:mt-0 transition ease-in duration-200 uppercase rounded-full hover:bg-sky-400 hover:text-white border-2 border-gray-900 focus:outline-none">
-                                    Add to cart
-                                  </button>
-                                </div>
+                                <button className="px-6 py-2 mt-2 md:mt-0 transition ease-in duration-200 uppercase rounded-full hover:bg-sky-400 hover:text-white border-2 border-gray-900 focus:outline-none">
+                                  Add to cart
+                                </button>
                               </div>
                             </div>
-                          </Link>
+                          </div>
                         </div>
                       ))}
                     </div>
